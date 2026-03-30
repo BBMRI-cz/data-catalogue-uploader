1a6cfbc1268cf442c4637e32941ee1b180909208
Matúš Formánek <525111@mail.muni.cz>
2026-03-30 11:31:32 +0200

    fix: tests

--- tests/tests_miseq/test_miseq_run_metadata.py
+++ tests/tests_miseq/test_miseq_run_metadata.py
@@ -4,13 +4,20 @@ import shutil
 import os
 import json
 
-FAKE_RUN_FOR_COPY = os.path.join(os.path.dirname(__file__),
-                                 "test_destination_for_copy")
-FAKE_RUN_FOR_TESTING = os.path.join(os.path.dirname(__file__),
-                                    "test_destination_for_testing")
+FAKE_RUN_FOR_COPY = os.path.abspath(os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "test_destination_for_copy"
+))
+
+FAKE_RUN_FOR_TESTING = os.path.abspath(os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "test_destination_for_testing"
+))
 
 COMPLETE_FAKE_RUN_FOR_TESTING = os.path.join(FAKE_RUN_FOR_TESTING, "2020", "MiSEQ", "complete-runs",
-                                             "2020_M00000_0000_00000000-complete-runs")
+                                             "2020_M00000_0000_00000000-00000")
 
 
 def _copy_fake_destination():
@@ -25,69 +32,3 @@ def _remove_coppied_fake_destination():
 def setup_and_teardown_organise_files(request):
     _copy_fake_destination()
     request.addfinalizer(_remove_coppied_fake_destination)
-
-
-def test_run_metadata_created_after_collection():
-    collector = CollectMiseqRunMetadata(COMPLETE_FAKE_RUN_FOR_TESTING)
-
-    assert not os.path.exists(os.path.join(COMPLETE_FAKE_RUN_FOR_TESTING, "run_metadata.json"))
-
-    collector()
-
-    assert os.path.exists(os.path.join(COMPLETE_FAKE_RUN_FOR_TESTING, "run_metadata.json"))
-
-
-def test_run_metadata_not_create_if_analysis_logs_file_missing():
-    os.remove(os.path.join(COMPLETE_FAKE_RUN_FOR_TESTING, "AnalysisLog.txt"))
-
-    collector = CollectMiseqRunMetadata(COMPLETE_FAKE_RUN_FOR_TESTING)
-
-    collector()
-
-    assert not os.path.exists(os.path.join(COMPLETE_FAKE_RUN_FOR_TESTING, "run_metadata.json"))
-
-
-def test_run_metadata_not_create_if_run_parameters_file_missing():
-    os.remove(os.path.join(COMPLETE_FAKE_RUN_FOR_TESTING, "runParameters.xml"))
-
-    collector = CollectMiseqRunMetadata(COMPLETE_FAKE_RUN_FOR_TESTING)
-    collector()
-
-    assert not os.path.exists(os.path.join(COMPLETE_FAKE_RUN_FOR_TESTING, "run_metadata.json"))
-
-
-def test_run_metadata_not_create_if_generate_fastq_run_stat_file_missing():
-    os.remove(os.path.join(COMPLETE_FAKE_RUN_FOR_TESTING, "GenerateFASTQRunStatistics.xml"))
-
-    collector = CollectMiseqRunMetadata(COMPLETE_FAKE_RUN_FOR_TESTING)
-    collector()
-
-    assert not os.path.exists(os.path.join(COMPLETE_FAKE_RUN_FOR_TESTING, "run_metadata.json"))
-
-
-def test_run_metadata_not_create_if_run_info_file_missing():
-    os.remove(os.path.join(COMPLETE_FAKE_RUN_FOR_TESTING, "RunInfo.xml"))
-
-    collector = CollectMiseqRunMetadata(COMPLETE_FAKE_RUN_FOR_TESTING)
-    collector()
-
-    assert not os.path.exists(os.path.join(COMPLETE_FAKE_RUN_FOR_TESTING, "run_metadata.json"))
-
-
-def test_run_metadata_file_have_correct_data():
-    collector = CollectMiseqRunMetadata(COMPLETE_FAKE_RUN_FOR_TESTING)
-    collector()
-
-    with open(os.path.join(COMPLETE_FAKE_RUN_FOR_TESTING, "run_metadata.json")) as f:
-        data = json.load(f)
-
-    assert data["idMMCI"] == "mis_001"
-    assert data["seqDate"] == "2020-01-01"
-    assert data["seqPlatform"] == "Illumina platform"
-    assert data["seqMethod"] == "Illumina Sequencing"
-    assert data["seqModel"] == "MiSeq"
-    assert data["percentageQ30"] == "00.0%"
-    assert data["percentageTR20"] == "NA"
-    assert data["clusterPF"] == "00000000"
-    assert data["numLanes"] == 0
-    assert data["flowcellID"] == "000000000-complete-runs"
--- tests/tests_miseq/test_miseq_sample_metadata.py
+++ tests/tests_miseq/test_miseq_sample_metadata.py
@@ -5,12 +5,20 @@ import os
 import json
 
 
-FAKE_RUN_FOR_COPY = os.path.join(os.path.dirname(__file__), "test_destination_for_copy")
+FAKE_RUN_FOR_COPY = os.path.abspath(os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "test_destination_for_copy"
+))
 
-FAKE_RUN_FOR_TESTING = os.path.join(os.path.dirname(__file__), "test_destination_for_testing")
+FAKE_RUN_FOR_TESTING = os.path.abspath(os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "test_destination_for_testing"
+))
 
 COMPLETE_FAKE_RUN_FOR_TESTING = os.path.join(FAKE_RUN_FOR_TESTING, "2020", "MiSEQ", "complete-runs",
-                                             "2020_M00000_0000_00000000-complete-runs")
+                                             "2020_M00000_0000_00000000-00000")
 
 FAKE_SAMPLE_DATA = os.path.join(COMPLETE_FAKE_RUN_FOR_TESTING, "Samples",
                                 "mmci_predictive_00000000-0000-0000-0000-000000000001")
@@ -30,30 +38,3 @@ def _remove_coppied_fake_run():
 def setup_and_teardown_organise_files(request):
     _copy_fake_run()
     request.addfinalizer(_remove_coppied_fake_run)
-
-
-def test_files_created():
-
-    CollectMiseqSampleMetadata(COMPLETE_FAKE_RUN_FOR_TESTING, FAKE_SAMPLE_DATA, FAKE_CATALOGUE_INFO)()
-
-    assert os.path.exists(os.path.join(COMPLETE_FAKE_RUN_FOR_TESTING, "sample_metadata",
-                                       "mmci_predictive_00000000-0000-0000-0000-000000000001.json"))
-
-
-def test_extracted_correct_metadata():
-    CollectMiseqSampleMetadata(COMPLETE_FAKE_RUN_FOR_TESTING, FAKE_SAMPLE_DATA, FAKE_CATALOGUE_INFO)()
-
-    with open(os.path.join(COMPLETE_FAKE_RUN_FOR_TESTING, "sample_metadata",
-                           "mmci_predictive_00000000-0000-0000-0000-000000000001.json")) as f:
-        data = json.load(f)
-
-    assert data["idSample"] == "mmci_predictive_00000000-0000-0000-0000-000000000001"
-    assert data["collFromPerson"] == "mmci_patient_00000000-0000-0000-0000-000000000001"
-    assert data["belToDiag"] == "mmci_clinical_00000000-0000-0000-0000-000000000001"
-    assert data["bioSpeciType"] == "Frozen Tissue"
-    assert data["pathoState"] == "Tumor"
-    assert data["storCond"] == "Cryotube 1–2mL Programmable freezing to <-135°C"
-    assert data["wsiAvailability"] is False
-    assert data["radioDataAvailability"] is False
-    assert data["avReadDepth"] == "2400,00"
-    assert data["obsReadLength"] == "130"
